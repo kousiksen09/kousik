a0290d399793e3ffd1a1134c65774e339f47750a
kousiksen09 <1704189@kiit.ac.in>
2020-12-30 19:13:10 +0530

    Updates

---
+++ precache-manifest.70c68d7a34ff23688c102f462efa6dd2.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0eafa1a7a1984ed7c985bc7a42249ab",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "ef98466ee86925c62084",
+    "url": "/portfolio/static/css/main.86563af6.chunk.css"
+  },
+  {
+    "revision": "6834e1e9bdc131e53dd9",
+    "url": "/portfolio/static/js/2.619f0083.chunk.js"
+  },
+  {
+    "revision": "a56315556249066f1eba5de93e7a16ef",
+    "url": "/portfolio/static/js/2.619f0083.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ef98466ee86925c62084",
+    "url": "/portfolio/static/js/main.741b928f.chunk.js"
+  },
+  {
+    "revision": "03c79f5677353ea2b056",
+    "url": "/portfolio/static/js/runtime-main.17dcb6df.js"
+  },
+  {
+    "revision": "7c251de8f19cff9b5ee5dbb9fdcb3f16",
+    "url": "/portfolio/static/media/Kousik_Resume.7c251de8.pdf"
+  },
+  {
+    "revision": "12da75d4aa795f25e2455c375f96dfe7",
+    "url": "/portfolio/static/media/automotive.12da75d4.png"
+  },
+  {
+    "revision": "95a7ca0045ac3182022691756488d65e",
+    "url": "/portfolio/static/media/github.95a7ca00.png"
+  },
+  {
+    "revision": "730310978cfe7b8d495af47eb3c3d443",
+    "url": "/portfolio/static/media/iccs.73031097.png"
+  },
+  {
+    "revision": "3b146b15a4aafed94fa29d711509f524",
+    "url": "/portfolio/static/media/kiitfest.3b146b15.jpg"
+  },
+  {
+    "revision": "0ff4c012f027068d97d75329aaaecee5",
+    "url": "/portfolio/static/media/prc.0ff4c012.jpg"
+  },
+  {
+    "revision": "0f5f50ada0e03e9a1acab8ce92c3f03b",
+    "url": "/portfolio/static/media/skillcrux.0f5f50ad.png"
+  }
+]);
