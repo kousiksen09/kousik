8d2453561b3c7bb766b129fbd9a9d498f5bf50a2
kousiksen09 <1704189@kiit.ac.in>
2020-12-31 09:32:32 +0530

    Fix | redux Devtool

--- src/Redux/store.js
+++ src/Redux/store.js
@@ -1,9 +1,6 @@
 import reducer from './reducer/reducer';
 import { createStore } from 'redux';
 
-export const store = createStore(
-  reducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__({ trace: true })
-);
+export const store = createStore(reducer);
 
 export default store;
